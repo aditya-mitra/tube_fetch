95862ea5980fa56177c3a86c9a519d9415be2a7a
aditya-mitra <ultrapokeinstinct@gmail.com>
2021-09-21 01:01:40 +0530

    [NEW] Setup and Configure Django Application
    commit ac5ed0b8794d0eb84d5f0f5709785dfed2489ea8
    Author: aditya-mitra <ultrapokeinstinct@gmail.com>
    Date:   Tue Sep 21 00:59:27 2021 +0530
    
        start default migrations and run server
    
    commit 4e1b27a26d77f2595196c72278ce887223e1282d
    Author: aditya-mitra <ultrapokeinstinct@gmail.com>
    Date:   Tue Sep 21 00:51:51 2021 +0530
    
        added the django settings and other configs
    
    commit 53b9557936cde154a1bbc100d69133ffc45104ec
    Author: aditya-mitra <ultrapokeinstinct@gmail.com>
    Date:   Tue Sep 21 00:31:24 2021 +0530
    
        Install django, drf, urllib3 and black(for formatting)

---
+++ server/tube_fetch_api/settings/__init__.py
@@ -0,0 +1,15 @@
+from dotenv import load_dotenv
+
+load_dotenv()
+
+from os import getenv
+
+IS_PRODUCTION = getenv("IS_PRODUCTION") is not None and getenv("IS_PRODUCTION").upper() == "TRUE"
+
+if IS_PRODUCTION:
+    print("The production server is running 🎉")
+    from .production import *
+else:
+    from .dev import *
+
+from .base import *
