9f57ac3fd8e23ad14c456c6d05214b4af75fddd4
aditya-mitra <ultrapokeinstinct@gmail.com>
2021-09-21 15:24:36 +0530

    configure baseweb design

--- dashboard/src/app.tsx
+++ dashboard/src/app.tsx
@@ -1,8 +1,25 @@
+import { Client as Styletron } from "styletron-engine-atomic";
+import { Provider as StyletronProvider } from "styletron-react";
+import { LightTheme, BaseProvider, styled } from "baseui";
+import Results from "./views/Results";
 
-export function App() {
-  return (
-    <>
-      <h1>workign</h1>
-    </>
-  )
+const engine = new Styletron();
+
+const Centered = styled("div", {
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    height: "100%",
+});
+
+export default function App() {
+    return (
+        <StyletronProvider value={engine}>
+            <BaseProvider theme={LightTheme}>
+                <Centered>
+                    <Results />
+                </Centered>
+            </BaseProvider>
+        </StyletronProvider>
+    );
 }
---
+++ dashboard/src/views/Results.tsx
@@ -0,0 +1,5 @@
+import YoutubeCard from "../components/card";
+
+export default function Results() {
+    return <YoutubeCard />;
+}
