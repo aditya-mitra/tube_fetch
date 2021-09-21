c5986f77346eb6cd68bcb735fcde0956a9d6a2e7
aditya-mitra <ultrapokeinstinct@gmail.com>
2021-09-21 12:32:53 +0530

    [IMPROVE] Add Search Functionality for Title and Description
    commit 8d04d8d3b819daee4412bdb9e5af73dcd17660e9
    Author: aditya-mitra <ultrapokeinstinct@gmail.com>
    Date:   Tue Sep 21 12:32:40 2021 +0530
    
        add search filter backend

--- server/results/views.py
+++ server/results/views.py
@@ -1,3 +1,4 @@
+from rest_framework.filters import SearchFilter
 from rest_framework.generics import ListAPIView
 
 from results.models import YTResult
@@ -7,3 +8,5 @@ from results.serializers import YTResultsSerializer
 class YTResultsListView(ListAPIView):
     serializer_class = YTResultsSerializer
     queryset = YTResult.objects.all()
+    filter_backends = [SearchFilter]
+    search_fields = ['title','description']
