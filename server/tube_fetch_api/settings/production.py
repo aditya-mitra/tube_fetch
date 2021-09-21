b53c3548ea5fd26a485ac45262b46a4491372bd0
aditya-mitra <ultrapokeinstinct@gmail.com>
2021-09-21 23:36:30 +0530

    configure postgres for production

---
+++ server/tube_fetch_api/settings/production.py
@@ -0,0 +1,22 @@
+from os import getenv
+from .base import BASE_DIR
+
+DEBUG = False
+
+SECRET_KEY = getenv('DJANGO_SECRET_KEY')
+
+ALLOWED_HOSTS = [
+    "localhost",
+    "127.0.0.1",
+]
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": getenv("DB_NAME"),
+        "USER": getenv("DB_USER"),
+        "PASSWORD": getenv("DB_PASSWORD"),
+        "HOST": getenv("DB_HOST", "127.0.0.1"),
+        "PORT": getenv("DB_PORT"),
+    }
+}
