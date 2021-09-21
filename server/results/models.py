2936aa42e31c95d19ed8ab1b6f073e81dbd3f6fc
aditya-mitra <ultrapokeinstinct@gmail.com>
2021-09-21 12:46:34 +0530

    add yt api keys model

--- server/results/models.py
+++ server/results/models.py
@@ -7,3 +7,8 @@ class YTResult(models.Model):
     thumbnail_url = models.URLField()
     description = models.TextField()
     published_date = models.DateTimeField(db_index=True)
+
+
+class YoutubeAPIKeys(models.Model):
+    api_key = models.CharField(max_length=200, unique=True)
+    name = models.CharField(max_length=100, blank=True)
--- server/results/views.py
+++ server/results/views.py
@@ -8,8 +8,6 @@ from results.serializers import YTResultsSerializer
 class YTResultsListView(ListAPIView):
     serializer_class = YTResultsSerializer
     queryset = YTResult.objects.all()
-    filter_backends = (SearchFilter,OrderingFilter)
+    filter_backends = (SearchFilter, OrderingFilter)
     search_fields = ("title", "description")
-    ordering_fields = ("published_date","title")
-
-
+    ordering_fields = ("published_date", "title")
